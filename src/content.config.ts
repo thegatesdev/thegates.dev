8b3d43fd26fab883174483c9b92e305065dd810f
thegatesdev <69715898+thegatesdev@users.noreply.github.com>
2025-06-26 17:09:28 +0200

    feat: allow mdx in article collection

--- src/content.config.ts
+++ src/content.config.ts
@@ -2,7 +2,7 @@ import { glob } from "astro/loaders";
 import { defineCollection, z } from "astro:content";
 
 const article = defineCollection({
-  loader: glob({ base: "./src/content/articles", pattern: "**/*.md" }),
+  loader: glob({ base: "./src/content/articles", pattern: "**/*.{md,mdx}" }),
   schema: z.object({
     title: z.string(),
     description: z.string(),
