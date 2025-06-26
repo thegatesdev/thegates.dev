30333a0df48da3892669be5405d02baf3f485a4a
thegatesdev <69715898+thegatesdev@users.noreply.github.com>
2025-06-26 19:21:51 +0200

    feat: create project collection

--- src/content.config.ts
+++ src/content.config.ts
@@ -13,4 +13,15 @@ const article = defineCollection({
   }),
 });
 
-export const collections = { article };
+const project = defineCollection({
+  loader: glob({ base: "./src/content/projects", pattern: "**/*.{md,mdx}" }),
+  schema: z.object({
+    title: z.string(),
+    description: z.string(),
+    started: z.date(),
+    ended: z.date().optional(),
+    url: z.string().url(),
+  }),
+});
+
+export const collections = { article, project };
