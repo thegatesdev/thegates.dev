335a915cf087cbe942f5575f41a1ce76bbef4a56
thegatesdev <69715898+thegatesdev@users.noreply.github.com>
2025-07-04 13:22:05 +0200

    Rework content lib, split featured and normal posts

--- src/content.lib.ts
+++ src/content.lib.ts
@@ -1,30 +1,38 @@
-import type { CollectionEntry } from "astro:content";
+import { getCollection, type CollectionEntry } from "astro:content";
 
 type Post = CollectionEntry<"post">;
 type Tag = CollectionEntry<"tag">;
 
-export function viewPostFilter(post: Post) {
-  const isPublished = post.data.published !== undefined;
-  return isPublished;
+export async function getViewablePosts() {
+  return await getCollection("post", ({ data }) => data.published != null);
 }
 
-function timeOrZero(date: Date | undefined): number {
-  return date?.getTime() ?? 0;
+export function splitFeaturedNormalPosts(posts: Post[]): [Post[], Post[]] {
+  const featured: Post[] = [];
+  const normal: Post[] = [];
+  for (const post of posts)
+    (post.data.featured == null ? normal : featured).push(post);
+  return [featured, normal];
 }
 
-export function viewPostSort(a: Post, b: Post) {
-  const featured = (a.data.featured ?? 1e9) - (b.data.featured ?? 1e9);
-  const date = timeOrZero(b.data.published) - timeOrZero(a.data.published);
-  return featured || date;
+export function filterPostsByTag({ id }: Tag) {
+  return (post: Post) => post.data.tags?.find((tag) => id === tag.id) != null;
 }
 
-export function viewTagSort(a: Tag, b: Tag) {
+export function sortPostsByFeatured(a: Post, b: Post) {
+  return (a.data.featured ?? 1e9) - (b.data.featured ?? 1e9);
+}
+
+export function sortPostsByPublished(a: Post, b: Post) {
+  return (
+    (b.data.published?.getTime() ?? 0) - (a.data.published?.getTime() ?? 0)
+  );
+}
+
+export async function getTags() {
+  return await getCollection("tag");
+}
+
+export function sortTagsByName(a: Tag, b: Tag) {
   return a.id.localeCompare(b.id);
-}
-
-export function viewPostsWithTagFilter({ id }: Tag) {
-  return (post: Post) => {
-    if (!viewPostFilter(post) || post.data.tags === undefined) return false;
-    return post.data.tags.find((tag) => id === tag.id);
-  };
 }
